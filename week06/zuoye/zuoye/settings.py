aaa3ce3ab300f37bbfd5a23b5cc4c740865296f9
chaizhenhua <chaizhenhua@mockuai.com>
2020-08-04 14:29:33 +0800

    提交作业

--- week06/zuoye/zuoye/settings.py
+++ week06/zuoye/zuoye/settings.py
@@ -128,6 +128,4 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
-STATIC_URL = '/statics/'
-
-STATICFILES_DIR = [os.path.join(BASE_DIR, 'statics'), ]
+STATIC_URL = '/static/'
